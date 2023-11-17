be2b2a727533c7126e035384f0ec0e359eafce8d
Alons08 <u21232269@utp.edu.pe>
2023-11-17 10:58:56 -0500

    Añadiendo el alert para eliminar usuarios

--- src/main/java/com/nibble/chinecas/controller/RolController.java
+++ src/main/java/com/nibble/chinecas/controller/RolController.java
@@ -1,8 +1,6 @@
 package com.nibble.chinecas.controller;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ src/main/resources/static/UsuarioFunctions.js
@@ -0,0 +1,28 @@
+function eliminar(login){
+    swal({
+        title: "¿Está seguro de eliminar?",
+        text: "Una vez eliminado, ¡no podrá recuperar este registro!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((OK) => {
+        if (OK) {
+            $.ajax({
+                url:"/usuario/eliminar/"+login,
+                success: function(res){
+                    console.log(res);
+                }
+            });
+          swal("¡Puf! ¡Su registro ha sido eliminado!", {
+            icon: "success",
+          }).then((ok)=>{
+              if(ok){
+                  location.href="/usuario";
+              }
+          });
+        } else {
+          swal("¡Su registro está seguro!");
+        }
+      });
+}
