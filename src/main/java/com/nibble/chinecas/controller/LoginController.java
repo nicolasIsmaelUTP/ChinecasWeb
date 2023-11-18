d983ee262cc37f9c196eadcb03c89fed881d034b
Nicolas Alayo <n_alayo_arias@outlook.com>
2023-11-18 08:54:52 -0500

    Remove unnecessary code and update login functionality

--- src/main/java/com/nibble/chinecas/controller/LoginController.java
+++ src/main/java/com/nibble/chinecas/controller/LoginController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.nibble.chinecas.model.Usuario;
 import com.nibble.chinecas.service.UsuarioService;
 
-
 @Controller
 @RequestMapping("")
 public class LoginController {
@@ -18,20 +17,27 @@ public class LoginController {
     @Autowired
     private UsuarioService usuarioService;
 
+    // Método para manejar la solicitud GET en la ruta raíz
     @GetMapping
     public String login(Model model){
         model.addAttribute("usuario", new Usuario());
-        return "/core/login2"; //borrar el 2
+        return "/core/login";
     }
 
-    @PostMapping("/index")
+    // Método para manejar la solicitud POST en la ruta raíz
+    @PostMapping
     public String autenticar(Usuario usuario, Model model){
+        // Autenticar al usuario utilizando el servicio de usuario
         Usuario usuarioAutenticado = usuarioService.autenticar(usuario);
         if(usuarioAutenticado != null){
+            // Si el usuario es autenticado, agregarlo al modelo y redirigir a la página de inicio
             model.addAttribute("usuario", usuarioAutenticado);
             return "/core/index";
         }
-        return "redirect:/";
+        // Si la autenticación falla, agregar un mensaje de error al modelo y redirigir a la página de inicio de sesión
+        model.addAttribute("error", "Usuario o contraseña incorrectos");
+        return "core/login";
     }
 
 }
+
