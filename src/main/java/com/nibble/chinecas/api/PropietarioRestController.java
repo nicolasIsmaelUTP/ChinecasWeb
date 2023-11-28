55e242607d67ce747ef6db4b2d3c969a81af5b0e
Nicolas Alayo <n_alayo_arias@outlook.com>
2023-11-28 11:48:56 -0500

    Add endpoints to retrieve individual propietarios, agricultores, and empresas

--- src/main/java/com/nibble/chinecas/api/PropietarioRestController.java
+++ src/main/java/com/nibble/chinecas/api/PropietarioRestController.java
@@ -3,9 +3,11 @@ package com.nibble.chinecas.api;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +17,6 @@ import com.nibble.chinecas.model.Propietario;
 import com.nibble.chinecas.service.AgricultorService;
 import com.nibble.chinecas.service.EmpresaService;
 import com.nibble.chinecas.service.PropietarioService;
-import java.util.TreeMap;
 
 @RestController
 @RequestMapping("/api/propietarios")
@@ -76,6 +77,51 @@ public class PropietarioRestController {
     }
 
     /**
+        * Obtiene un objeto Propietario según su identificador.
+        * 
+        * @param id El identificador del propietario.
+        * @return Un objeto que representa al propietario, o null si no se encuentra.
+        */
+    @GetMapping("/{id}")
+    public Object obtener(@PathVariable("id") String id) {
+        Propietario propietario = propietarioService.obtener(id).orElse(null);
+        if (propietario != null) {
+            Map<String, Object> propietarioMap = new TreeMap<>();
+            propietarioMap.put("tipo", propietario.getTipo());
+            propietarioMap.put("direccion", propietario.getDireccion());
+            propietarioMap.put("correo", propietario.getCorreo());
+            propietarioMap.put("telefono", propietario.getTelefono());
+
+            // Si el propietario es de tipo "natural"
+            if (propietario.getTipo().equals("natural")) {
+                Agricultor agricultor = agricultorService.obtener(propietario.getId()).orElse(null);
+                if (agricultor != null) {
+                    propietarioMap.put("numDoc", agricultor.getNumDoc());
+                    propietarioMap.put("primerNombre", agricultor.getPrimerNombre());
+                    propietarioMap.put("segundoNombre", agricultor.getSegundoNombre());
+                    propietarioMap.put("apellidoPaterno", agricultor.getApellidoPaterno());
+                    propietarioMap.put("apellidoMaterno", agricultor.getApellidoMaterno());
+                    propietarioMap.put("tipoDocumento", agricultor.getTipoDocumento().getNombre());
+                }
+            }
+            // Si el propietario es de tipo "empresa"
+            else {
+                Empresa empresa = empresaService.obtener(propietario.getId()).orElse(null);
+                if (empresa != null) {
+                    propietarioMap.put("ruc", empresa.getRuc());
+                    propietarioMap.put("nombre", empresa.getNombre());
+                    propietarioMap.put("razonSocial", empresa.getRazonSocial());
+                    propietarioMap.put("tipoDeEmpresa", empresa.getTipoDeEmpresa().getNombre());
+                }
+            }
+
+            return propietarioMap;
+        }
+
+        return null;
+    }
+
+    /**
      * Obtiene todos los agricultores.
      * 
      * @return ArrayList con los agricultores y sus detalles.
@@ -102,6 +148,32 @@ public class PropietarioRestController {
     }
 
     /**
+        * Obtiene un objeto Agricultor a partir del número de documento.
+        * 
+        * @param numDoc el número de documento del agricultor
+        * @return un objeto que representa al agricultor, o null si no se encuentra
+        */
+    @GetMapping("/agricultores/{numDoc}")
+    public Object obtenerAgricultor(@PathVariable("numDoc") String numDoc) {
+        Agricultor agricultor = agricultorService.obtener(numDoc).orElse(null);
+        if (agricultor != null) {
+            Map<String, Object> agricultorMap = new TreeMap<>();
+            agricultorMap.put("numDoc", agricultor.getNumDoc());
+            agricultorMap.put("primerNombre", agricultor.getPrimerNombre());
+            agricultorMap.put("segundoNombre", agricultor.getSegundoNombre());
+            agricultorMap.put("apellidoPaterno", agricultor.getApellidoPaterno());
+            agricultorMap.put("apellidoMaterno", agricultor.getApellidoMaterno());
+            agricultorMap.put("tipoDocumento", agricultor.getTipoDocumento().getNombre());
+            agricultorMap.put("direccion", agricultor.getPropietario().getDireccion());
+            agricultorMap.put("correo", agricultor.getPropietario().getCorreo());
+            agricultorMap.put("telefono", agricultor.getPropietario().getTelefono());
+            return agricultorMap;
+        }
+
+        return null;
+    }
+
+    /**
      * Obtiene todas las empresas.
      * 
      * @return ArrayList con las empresas y sus detalles.
@@ -125,4 +197,30 @@ public class PropietarioRestController {
         return empresas;
     }
 
+    /**
+        * Obtiene la información de una empresa dado su RUC.
+        * 
+        * @param ruc el RUC de la empresa
+        * @return un objeto que contiene la información de la empresa, o null si no se encuentra
+        */
+    @GetMapping("/empresas/{ruc}")
+    public Object obtenerEmpresa(@PathVariable("ruc") String ruc) {
+        Empresa empresa = empresaService.obtener(ruc).orElse(null);
+        if (empresa != null) {
+            Map<String, Object> empresaMap = new TreeMap<>();
+            empresaMap.put("ruc", empresa.getRuc());
+            empresaMap.put("nombre", empresa.getNombre());
+            empresaMap.put("razonSocial", empresa.getRazonSocial());
+            empresaMap.put("tipoDeEmpresa", empresa.getTipoDeEmpresa().getNombre());
+            empresaMap.put("direccion", empresa.getPropietario().getDireccion());
+            empresaMap.put("correo", empresa.getPropietario().getCorreo());
+            empresaMap.put("telefono", empresa.getPropietario().getTelefono());
+            return empresaMap;
+        }
+
+        return null;
+    }
+
+    // @GetMapping("/correo/{correo}")
+    // public ArrayList<Object> obtenerPorCorreo
 }
