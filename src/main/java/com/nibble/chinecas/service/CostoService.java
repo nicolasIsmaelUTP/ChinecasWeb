f7ffd4982242ed22161e42b08a86dc263378bd45
Nicolas Alayo <n_alayo_arias@outlook.com>
2023-12-06 11:22:38 -0500

    Add methods to obtain available costs for a specific terrain and year

--- src/main/java/com/nibble/chinecas/service/CostoService.java
+++ src/main/java/com/nibble/chinecas/service/CostoService.java
@@ -2,11 +2,14 @@ package com.nibble.chinecas.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nibble.chinecas.model.Costo;
+import com.nibble.chinecas.model.Lectura;
+import com.nibble.chinecas.model.Terreno;
 import com.nibble.chinecas.repository.ICostoRepository;
 
 @Service
@@ -40,4 +43,26 @@ public class CostoService implements IService<Costo, Integer> {
         data.deleteById(id);
     }
     
+    /**
+     * Obtiene la lista de costos disponibles para un terreno en un año específico.
+     * 
+     * @param terreno El terreno para el cual se obtendrán los costos disponibles.
+     * @param anio El año para el cual se obtendrán los costos disponibles.
+     * @return La lista de costos disponibles para el terreno en el año especificado.
+     */
+    public List<Costo> obtenerCostosDisponiblesParaTerrenoEnAnio(Terreno terreno, int anio) {
+        // Todos los costos
+        List<Costo> costos = obtenerTodos();
+
+        // Costos de lecturas ya realizadas
+        List<Costo> costosDeLecturas = new LecturaService().obtenerLecturasPorTerrenoYAnio(terreno, anio)
+            .stream()
+            .map(Lectura::getCosto)
+            .collect(Collectors.toList());
+
+        // Costos disponibles
+        return costos.stream()
+            .filter(costo -> !costosDeLecturas.contains(costo))
+            .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/nibble/chinecas/service/LecturaService.java
+++ src/main/java/com/nibble/chinecas/service/LecturaService.java
@@ -1,14 +1,17 @@
 package com.nibble.chinecas.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nibble.chinecas.api.ItemRecibo;
 import com.nibble.chinecas.model.Lectura;
+import com.nibble.chinecas.model.Terreno;
 import com.nibble.chinecas.repository.ILecturaRepository;
 
 import jakarta.persistence.EntityManager;
@@ -49,7 +52,30 @@ public class LecturaService implements IService<Lectura, Integer> {
         data.deleteById(id);
     }
 
-    
+    /**
+     * Obtiene una lista de lecturas filtradas por terreno y año.
+     *
+     * @param terreno el terreno por el cual filtrar las lecturas
+     * @param anio el año por el cual filtrar las lecturas
+     * @return una lista de lecturas filtradas por terreno y año
+     */
+    public List<Lectura> obtenerLecturasPorTerrenoYAnio(Terreno terreno, int anio) {
+        List<Lectura> lecturas = obtenerTodos();
+
+        return lecturas.stream()
+                .filter(lectura -> lectura.getTerreno().equals(terreno))
+                .filter(lectura -> Calendar.getInstance().get(Calendar.YEAR) == anio)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Obtiene una lista de items de recibo para un terreno y año específico.
+     * 
+     * @param terreno_codigo El código del terreno.
+     * @param anio           El año para el cual se desea obtener los items.
+     * @return Una lista de objetos ItemRecibo que representan los items de recibo
+     *         encontrados.
+     */
     public List<ItemRecibo> obtenerItems(int terreno_codigo, int anio) {
         String sql = "SELECT c.descripcion, YEAR(l.fecha), c.valor_metro_cubico, l.volumen_utilizado, l.id FROM lectura l INNER JOIN costo c ON l.costos_id = c.id WHERE l.terreno_codigo = :terreno_codigo AND YEAR(l.fecha) = :anio";
 
@@ -66,7 +92,8 @@ public class LecturaService implements IService<Lectura, Integer> {
             double valor_metro_cubico = (double) result[2];
             double volumen_utilizado = (double) result[3];
             int lectura_id = (int) result[4];
-            ItemRecibo itemRecibo = new ItemRecibo(descripcion, year, valor_metro_cubico, volumen_utilizado, lectura_id);
+            ItemRecibo itemRecibo = new ItemRecibo(descripcion, year, valor_metro_cubico, volumen_utilizado,
+                    lectura_id);
             itemRecibos.add(itemRecibo);
         }
 
